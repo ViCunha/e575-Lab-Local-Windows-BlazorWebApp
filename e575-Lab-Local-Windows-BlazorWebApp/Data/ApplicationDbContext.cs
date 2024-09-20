22df886013624335132edea203d0f7a1d7cb7a75
Vinicius Martins Cunha <vicunha@outlook.com>
2024-09-20 22:53:16 +0100

    Implement the render modes: static, server, WebAssembly, and auto on specific pages as needed

--- e575-Lab-Local-Windows-BlazorWebApp/Data/ApplicationDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace e575_Lab_Local_Windows_BlazorWebApp.Data
-{
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
-    {
-    }
-}
